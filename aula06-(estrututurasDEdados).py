538d08259fa923dbc0bf1414fd40b325242a61c9
Elenilton1 <127469665+Elenilton1@users.noreply.github.com>
2023-03-29 02:04:20 -0300

    Delete aula06-(estrututurasDEdados).py

--- aula06-(estrututurasDEdados).py
+++
@@ -1,7 +0,0 @@
-minha_lista=["junior","arieli","thor"] #Lista/List
-minha_tupla=("junior","arieli","thor") #Tupla/Tuple
-meu_dict={"junior":"esposo","arieli":"esposa","thor":"cachorro"} #Dicionário/Dict
-meu_conjunto={"junior","arieli","thor",} #Conjunto/Set
-meu_dict["endereço"]="al-dos pinheiros"
-loucura=[(1,2),(3,4),(5,6),({"junior","arieli"}),{"esposo":"junior"}]
-print(meu_dict["junior"])
