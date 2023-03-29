c7f9111db558ddd5596b59eb7b100b1d7442f9c0
Elenilton1 <127469665+Elenilton1@users.noreply.github.com>
2023-03-29 02:05:16 -0300

    Delete aula10-entrada-e-saida-de-arq.py

--- aula10-entrada-e-saida-de-arq.py
+++
@@ -1,6 +0,0 @@
-arquivo=open("imagem.png","rb")
-'''for i in range (0,102,2):#range permite especificar o inicio de uma sequencia, final e o salto.
-  type(print(arquivo))
-  arquivo.write("bunda "+str(i)+"\n") #escrever dentro do arquivo #tem que ser o + invés de virgula.
-print(arquivo.read())'''
-print(arquivo.read())
